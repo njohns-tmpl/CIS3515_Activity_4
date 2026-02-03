9231e10fd1f67b5dfea6269e583084c2f6d59ac2
njohns-tmpl <tuq84855@temple.edu>
2026-02-03 11:47:17 -0500

    ToDo 2, 3, & 4 completed 1 is still unresolved

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -14,13 +14,23 @@ class MainActivity : AppCompatActivity() {
 
         /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
         //val numberArray = Array Of Numbers
+        val numberArray = Array<Int>(20) { index -> (index * 5) + 5}
 
         /* TODO Step 3: Create adapter to display items from array in Spinner */
         //spinner.adapter = ArrayAdapter...
-
-
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, numberArray)
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
         //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>, p1: View?, p2: Int, p3: Long) {
+                val selectedSize = p0.getItemAtPosition(p2).toString().toFloat()
+
+                displayTextView.textSize = selectedSize
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
 
     }
 }
